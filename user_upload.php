85c16532a1307380d7edef9a3262545cabb24be7
Steve Torrens <stevetorrens70@gmail.com>
2018-12-07 12:18:18 +1300

    Started working on --help

--- user_upload.php
+++ user_upload.php
@@ -1,4 +1,30 @@
 #!/Applications/MAMP/bin/php/php7.0.32/bin/php
 <?php
- echo "helloworld\n"
+$shortopts = "";
+$longopts  = array(
+  "help"           // No value
+);
+$options = getopt($shortopts, $longopts);
+var_dump($options);
+switch ($options["help"])
+{
+  case false;
+    directives_list();
+    break;
+}
+
+function directives_list(){
+ echo "
+  --file [csv file name] – this is the name of the CSV to be parsed\n
+  --create_table – this will cause the MySQL users table to be built (and no further
+  action will be taken)\n
+  --dry_run – this will be used with the --file directive in the instance
+  that we want to run the script but not insert into the DB. All other functions
+  will be executed, but the database won't be altered\n
+  -u – MySQL username\n
+  -p – MySQL password\n
+  -h – MySQL host\n
+  --help – which will output the above list of directives with details\n";
+}
+
 ?>
