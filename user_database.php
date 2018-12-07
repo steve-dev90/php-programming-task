1bb4af050388a276d512184871dbb2bae801e395
Steve Torrens <stevetorrens70@gmail.com>
2018-12-08 10:03:05 +1300

    Working on create_table object

--- user_database.php
+++ user_database.php
@@ -1,33 +1,60 @@
 #!/Applications/MAMP/bin/php/php7.0.32/bin/php
 <?php
 
-  DEFINE('DB_USERNAME', 'root');
-  DEFINE('DB_PASSWORD', 'root');
-  DEFINE('DB_PORT', '8889');
-  DEFINE('DB_HOST', '127.0.0.1');
-  DEFINE('DB_DATABASE', 'progtask');
+DEFINE('DB_USERNAME', 'root');
+DEFINE('DB_PASSWORD', 'root');
+DEFINE('DB_PORT', '8889');
+DEFINE('DB_HOST', '127.0.0.1');
+DEFINE('DB_DATABASE', 'progtask');
 
-  $mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
+class CreateUsersTable {
 
-  if (mysqli_connect_error()) {
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-  }
+  public function __construct() {
+    $this->$mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_PORT);
 
-  // Attempt create table query execution
-  $sql = "CREATE TABLE users(
-    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(30) NOT NULL,
-    surname VARCHAR(30) NOT NULL,
-    email VARCHAR(70) NOT NULL
-  )";
-
-  if(mysqli_query($mysqli, $sql)){
-    echo "Table created successfully.";
-  } else{
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($mysqli);
-  }
+    if (mysqli_connect_error()) {
+      die("ERROR: Could not connect. " . mysqli_connect_error());
+    }
     echo 'Connected successfully.';
+  }
 
-  $mysqli->close();
+  public function createUsersTable() {
+    // Attempt create table query execution
+    $sql = "CREATE TABLE users(
+      id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+      first_name VARCHAR(30) NOT NULL,
+      surname VARCHAR(30) NOT NULL,
+      email VARCHAR(70) NOT NULL
+    )";
+
+    if(mysqli_query($this->$mysqli, $sql)){
+      echo "Table created successfully.";
+    } else{
+      echo "ERROR: Could not execute $sql. " . mysqli_error($this->$mysqli);
+    }
+  }
+
+  public function insertUser() {
+    // Attempt insert query execution
+    $sql = "INSERT INTO users (first_name, surname, email) VALUES ('Peter', 'Parker', 'peterparker@mail.com')";
+    if(mysqli_query($this->$mysqli, $sql)){
+      echo "Records inserted successfully.";
+    } else{
+      echo "ERROR: Could not execute $sql. " . mysqli_error($this->$mysqli);
+    }
+  }
+
+  public function __destruct() {
+    $this->$mysqli->close();
+  }
+}
+
+$obj = new CreateUsersTable();
+$obj->createUsersTable();
+$obj->insertUser();
+
+
+
+
 ?>
 
